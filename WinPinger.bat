1dacfcbec753296c04784467887923b418fa866e
Decation2 <88938287+Decation2@users.noreply.github.com>
2022-04-15 10:53:45 +0500

    Update WinPinger.bat

--- WinPinger.bat
+++ WinPinger.bat
@@ -1,7 +1,7 @@
 @echo off
 title WinPinger
 color 0a
-echo  Welcome to the WinPinger!
-set /p ping="Type here to the ping:"
+echo Welcome to the WinPinger!
+set /p ping="Type here to the ping the server or ip address:"
 echo Now you going to ping the %ping%
 ping %ping% -n 900
